0636a8e4a710e0389c49b5f55ac0eac45537571f
Axthelm <axthelm@Axthelms-MBP.home>
2019-03-07 17:44:06 -0800

    reformat... loads image okay, testing

--- src/main/java/Multiplex_Pixel_Colocalization_.java
+++ src/main/java/Multiplex_Pixel_Colocalization_.java
@@ -7,68 +7,63 @@ import ij.process.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 
 
 
 public class Multiplex_Pixel_Colocalization_ implements PlugIn 
 {
   static String title = "Multiplex Pixel Colocalization";
-  static final int validChannelMinCount = 2;
+  static final int validChannelMinCount = 1;
   static final int validChannelMaxCount = 8;
   static final String POSITIVE = "(+)";
   static final String NEGATIVE = "(-)";
 
-  int channelCount = 0;
-  double[] channelThreshold = null;
-  String[] channelTitles = null;
-  ImagePlus[] inputImages = null;
-  ImagePlus outputImage;
-  ImageStack outputImageStack; 
-  int[] colocalizationCounts = null; //count by picture/stack
-  int width = 0, height = 0, stackSize = 0;
+  int stampCount = 0; /* Number of Image Stacks to import and analyze*/
+  String[] imageTitles = null;
+  ImagePlus[] inputImages = null; 
+  double[] channelThresholds = null;
+  int comboOptions = 0;
+  int[][] colocalizationCounts = null; //count by picture/stack
+  int width = 0, height = 0, sliceCount = 0;
   String outputfilename = "colocalizationCounts.csv";
 
   public void run(String arg) 
   {
-    int[] openWindowList = null; //List of all open windows
-    String[] imageTitleList = null;
-    
-    /* Check start conditions */
+    /* Check start conditions, don't know if necessary */
     if (IJ.versionLessThan("1.27w")) 
     {
       return;
     }
 
-    /* Get the number of channels to analyze, and error check the input */
-    openWindowList = WindowManager.getIDList();
-    if (openWindowList == null)
-    {
-      IJ.showMessage(title, "ERR: Please open all channels and images you would like to analyze.");
-      return;
-    }
-    imageTitleList = getImageOptions(openWindowList);
-    channelCount = returnChannelCount(imageTitleList.length);
-    if (channelCount == 0)
+    /* How many image stacks will the user upload*/
+    stampCount = returnStampCount();
+    if (stampCount == 0)
     {
       return;
     }
- 
+
     /* Set up arrays based on channels selected by user*/
-    inputImages = new ImagePlus[channelCount];
-    channelThreshold = new double[channelCount];
-    //There are 2^(#images) options for overlap
-    colocalizationCounts = new int[(int)Math.pow(2, channelCount)]; 
-    
-    
-    /*Get info on channels to analyze*/
-    if (!getChannels(openWindowList)) 
+    inputImages = new ImagePlus[stampCount];
+    channelThresholds = new double[stampCount];
+
+    /* Get Input Images, Check Sizes, Apply ROI Masks*/
+    if (!getStampsPostRoiMask()) 
     {
+      IJ.showMessage(title, "Error Getting Images");
       return; 
     }
-    outputImage = inputImages[0].duplicate();
-
+    
+    /* Hold 2^(#slices) for each image stamp*/
+    if (sliceCount == 0)
+    {
+      IJ.showMessage(title, "Slice Count is Zero. Unknown Error. Exiting.");
+      return;
+    }
+    comboOptions = (int)Math.pow(2, sliceCount);
+    colocalizationCounts = new int[stampCount][comboOptions]; 
+    
     calculateColocolization();
-    outputImage.show();
     if(!saveColocalizationCounts())
     {
       IJ.showMessage("ERR: Failed to save CSV file");
@@ -98,20 +93,15 @@ public class Multiplex_Pixel_Colocalization_ implements PlugIn
   }
 
 
-  /*
-   * This function prompts the user to enter the channel count
-   * that they would like to analyze. It validates the number is 
-   * between the minimum and maximum # of channels this plugin 
-   * can analyze and returns the value as an int.
-   * It also verifies correct number of image windows are open. 
-   * Return: # of channels to analyze, 0 on ERR or Exit
+  /* 
+   * Return: # of Image Stamps to analyze, 0 on ERR or Exit
    * */
-  private int returnChannelCount(int imagesOpen)
+  private int returnStampCount()
   {
     double count = 0; 
     GenericDialog gd = new GenericDialog(title);
-    gd.addMessage("Please make sure all channels you want to analyze are open.");
-    gd.addNumericField("Channel Count(2-6):", 2, 0);
+    gd.addMessage("This is the number of image stacks you would like to analyze together.");
+    gd.addNumericField("Image Stamp Count (whole number greater than zero):", 2, 0);
     gd.setCancelLabel("Exit");
     gd.showDialog();
     if (gd.wasCanceled()) 
@@ -119,23 +109,12 @@ public class Multiplex_Pixel_Colocalization_ implements PlugIn
       return (int)0;
     }
     count = gd.getNextNumber();
-    if (Double.isNaN(count))
-    {
-      IJ.showMessage(title, "ERR: Please enter a valid integer between (including):" + Integer.toString(validChannelMinCount) 
-      + " and " + Integer.toString(validChannelMaxCount));
-      return (int)0;
-    }
-    if (count > imagesOpen)
-    {
-      IJ.showMessage(title, "Please make sure all channels you want to analyze are open");
-      return (int)0;
-    }
-    if ((count < validChannelMinCount) || 
-        (count > validChannelMaxCount) ||
+    if ((Double.isNaN(count)) || 
+        (count < 0) ||
         (count != Math.ceil(count)))
     {
-      IJ.showMessage(title, "ERR: Channel Count must be an integer between " + Integer.toString(validChannelMinCount) 
-        + " and " + Integer.toString(validChannelMaxCount) + "  Count Entered: " + Double.toString(count));
+      IJ.showMessage(title, "ERR: Please enter a valid integer above zero:" + Integer.toString(validChannelMinCount) 
+      + " and " + Integer.toString(validChannelMaxCount));
       return (int)0;
     }
     return (int)count;
@@ -145,73 +124,156 @@ public class Multiplex_Pixel_Colocalization_ implements PlugIn
    * This will get the list of relevant channel names from the user
    * and validate they are the same size, before returning true or false
    * based on success or failure
+   * Count: number of input images the user selected.
    * */
   
-  private boolean getChannels(int[] openWindowList) 
+  private boolean getStampsPostRoiMask()
   {
-    String[] imagetitleoptions = getImageOptions(openWindowList);
-    GenericDialog gd = new GenericDialog(title);
-    for(int i = 0; i < channelCount; i++) 
+    if (Array.getLength(inputImages) != stampCount)
     {
-      String option = "Channel #" + Integer.toString(i) + ": ";
-      gd.addChoice(option, imagetitleoptions, imagetitleoptions[i]);
-      gd.addNumericField("Channel Threshold Value (0-255):", 50, 1);
-      gd.addMessage(" ");
-    }
-    gd.showDialog();
-    if (gd.wasCanceled()) 
-    {
-      return false;
+      IJ.showMessage(title, "ERR: Setup Failure. Image array length not equal to stampCount requested");
     }
     
-    int[] imageIndex = new int[channelCount];
-    for(int i = 0; i < channelCount; i++) 
+    for (int i=0; i < stampCount; i++)
     {
-      imageIndex[i] = gd.getNextChoiceIndex();
-      channelThreshold[i] = gd.getNextNumber();
-      inputImages[i] = WindowManager.getImage(openWindowList[imageIndex[i]]);
-  
-      /*Check images for consistent sizing*/
-      if (i == 0)
+      if(!getNextStamp(i))
       {
-        stackSize = inputImages[i].getStackSize();
-        height = inputImages[i].getHeight();
-        width = inputImages[i].getWidth();
-        if (inputImages[i].getType()!= ImagePlus.GRAY8)
-        {
-          IJ.showMessage(title, "Must be Gray8 type");
-          return false;
-        }
-      } 
-      else 
+        IJ.showMessage(title, "ERR: Unknown Error Getting Input Image");
+        return false;
+      }
+      if(!checkImageData(i))
       {
-        int newstackSize = inputImages[i].getStackSize();
-        int newheight = inputImages[i].getHeight();
-        int newwidth = inputImages[i].getWidth();
-        if((stackSize != newstackSize) || (height != newheight) || (width != newwidth))
-        {
-          String imagesize = "stack:" + Integer.toString(stackSize) +
-                             " height:" + Integer.toString(height) +
-                             " width:"+ Integer.toString(stackSize) +
-                             " new stack:" + Integer.toString(newstackSize) +
-                             " new height:" + Integer.toString(newheight) +
-                             " new width:"+ Integer.toString(newstackSize) +
-                             " Image Number: " + Integer.toString(i);
-          IJ.showMessage(title, "ERR: Image sizes must be the same" + imagesize);
-          return false;
-        }
-        if (inputImages[i].getType()!= ImagePlus.GRAY8)
-        {
-          IJ.showMessage(title, "Must be Gray8 type");
-          return false;
-        }
+        IJ.showMessage(title, "ERR with Image Data, Exiting");
+        return false;
+      }
+      if(!getApplyROI(i))
+      {
+        IJ.showMessage(title, "ERR failed to apply ROI, exiting.");
+        return false;
       }
     }
     return true;
   }
 
-  
-  private boolean doesthiscontain(Roi [] rois, int x, int y)
+  private boolean getNextStamp(int stampnumber)
+  {
+    /* Get the number of channels to analyze, and error check the input */
+    int [] openWindowList = WindowManager.getIDList();
+    if (openWindowList == null)
+    {
+      IJ.showMessage(title, "ERR: Please open images you would like to analyze.");
+    }
+    openWindowList = WindowManager.getIDList();
+    if (openWindowList == null)
+    {
+      IJ.showMessage(title, "ERR: Unable to generate image options. Exiting.");
+      return false; 
+    }
+    String [] imagetitleoptions = getImageOptions(openWindowList);
+    GenericDialog gd = new GenericDialog(title);
+    String option = "Channel: ";
+    gd.addChoice(option, imagetitleoptions, imagetitleoptions[0]);
+    gd.addNumericField("Channel Threshold Value (0-255):", 50, 1);
+    gd.showDialog();
+    if (gd.wasCanceled()) 
+    {
+      return false;
+    }
+    int imageIndex = gd.getNextChoiceIndex();
+    channelThresholds[stampnumber] = gd.getNextNumber();
+    inputImages[stampnumber] = WindowManager.getImage(openWindowList[imageIndex]);
+    if (inputImages[stampnumber] == null)
+    {
+      IJ.showMessage(title, "ERR: Error loading Image.");
+      return false; 
+    }
+   return true;
+  }
+
+  private boolean checkImageData(int i)
+  {
+    /*Check images for consistent sizing*/
+    if (i == 0)
+    {
+      sliceCount = inputImages[i].getStackSize();
+      height = inputImages[i].getHeight();
+      width = inputImages[i].getWidth();
+      if((sliceCount == 0) || (height == 0) || (width == 0))
+      {
+        String imagesize = "slices::" + Integer.toString(sliceCount) +
+                           " height:" + Integer.toString(height) +
+                           " width:"+ Integer.toString(width);
+        IJ.showMessage(title, "ERR: Image must exist. " + imagesize);
+      }
+      if (inputImages[i].getType()!= ImagePlus.GRAY8)
+      {
+        IJ.showMessage(title, "Must be Gray8 type");
+        return false;
+      }
+    } 
+    else 
+    {
+      int newSliceCount = inputImages[i].getStackSize();
+      int newheight = inputImages[i].getHeight();
+      int newwidth = inputImages[i].getWidth();
+      if((sliceCount != newSliceCount) || (height != newheight) || (width != newwidth))
+      {
+        String imagesize = "stack:" + Integer.toString(sliceCount) +
+                           " height:" + Integer.toString(height) +
+                           " width:"+ Integer.toString(width) +
+                           " new stack:" + Integer.toString(newSliceCount) +
+                           " new height:" + Integer.toString(newheight) +
+                           " new width:"+ Integer.toString(newwidth) +
+                           " Image Number: " + Integer.toString(i);
+        IJ.showMessage(title, "ERR: Image sizes must be the same. " + imagesize);
+        return false;
+      }
+      if (inputImages[i].getType()!= ImagePlus.GRAY8)
+      {
+        IJ.showMessage(title, "Must be Gray8 type");
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean getApplyROI(int stampNumber)
+  {
+    RoiManager roiMng = RoiManager.getRoiManager();
+    WaitForUserDialog wd_roid = new WaitForUserDialog("USER ROI SELECT", "Select ROI using ROI Manger. \nNote if you load an ROI set and select no-ROIs all ROIs will be auto-selected.");
+    wd_roid.show();
+    Roi[ ] rois = roiMng.getSelectedRoisAsArray();
+    if (rois.length == 0) 
+    {
+      IJ.showMessage(title,"NO ROI IN USE. Continue");
+      return true;
+    } 
+    else
+    {
+       IJ.showMessage(title, "Roi Count = " + Integer.toString(rois.length));
+    }
+    
+    ImageProcessor imageProcessor = inputImages[stampNumber].getProcessor();
+
+    for (int x=0; x<width; x++) 
+    {
+      for (int y=0; y<height; y++) 
+      {
+        if (roisContainPixel(rois,x,y) == false)
+        {
+          /*Note slice numbers start at 1*/
+          for (int n=1; n<=sliceCount; n++) 
+          {
+            inputImages[stampNumber].setSlice(n);
+            imageProcessor.putPixelValue(x, y, 0);
+          }
+        }
+      }   
+    }
+    return true;
+  }
+
+  private boolean roisContainPixel(Roi [] rois, int x, int y)
   {
      for (int i = 0; i < rois.length; i++)
      {
@@ -232,93 +294,54 @@ public class Multiplex_Pixel_Colocalization_ implements PlugIn
    * */
   private void calculateColocolization()
   {
-    int[] pixelLit = new int[channelCount + 1];
-    ImageProcessor [] imageProcessor = new ImageProcessor[channelCount + 1];
-    ImageStack[] stack = new ImageStack[channelCount + 1];
-    float[][] ctable = new float[channelCount + 1][];
-    RoiManager roiMng = RoiManager.getRoiManager();
-    
-    WaitForUserDialog wd_roid = new WaitForUserDialog("USER ROI SELECT", "Select Rois Please");
-    wd_roid.show();
-    Roi[ ] rois = roiMng.getSelectedRoisAsArray();
-    if (rois.length == 0) 
-    {
-      IJ.showMessage(title,"NO ROI IN USE. Continue");
-    } else
-    {
-     IJ.showMessage(title, "Using Rois = " + Integer.toString(rois.length));
-    }
+    ImageProcessor [] imageProcessor = new ImageProcessor[stampCount];
 
- 
-    //Grab information for each channel
-    for(int i=0; i<channelCount; i++)
+    for (int i=0; i<stampCount; i++)
     {
-      stack[i] = inputImages[i].getStack();
-      ctable[i] = inputImages[i].getCalibration().getCTable();
+      imageProcessor[i] = inputImages[i].getProcessor();
     }
     
+    int [][]colocalizationIndex = new int[stampCount][sliceCount]; //Allows selection of right count to increase.
 
-    stack[channelCount] = outputImage.getStack();
-    ctable[channelCount] = outputImage.getCalibration().getCTable();
-
-
-    for (int n=1; n<=stackSize; n++) 
+    for(int i=0; i<stampCount; i++)
     {
-      for (int i=0; i<(channelCount+1); i++)
+      for(int n=1; n<=sliceCount; n++)
       {
-        imageProcessor[i] = stack[i].getProcessor(n);
-        imageProcessor[i].setCalibrationTable(ctable[i]);
-      }
-      
-      for (int x=0; x<width; x++) 
-      {
-        for (int y=0; y<height; y++) 
+        inputImages[i].setSlice(n);
+        for (int x=0; x<width; x++) 
         {
-          if ((rois.length == 0) || ((rois.length != 0) && doesthiscontain(rois,x,y)))
+          for (int y=0; y<height; y++) 
           {
-          //Pixel Analysis 
-          int colocalizationIndex = 0x0; //Allows selection of right count to increase.
-          pixelLit[channelCount] = 1;
-
-          for(int i=0; i<channelCount; i++)
-          {
-            pixelLit[i] = (imageProcessor[i].getPixel(x,y) > channelThreshold[i])?1:0;
-            pixelLit[channelCount] = pixelLit[i] & pixelLit[channelCount];
-            colocalizationIndex = (colocalizationIndex << 1) + pixelLit[i]; 
-          }          
-          imageProcessor[channelCount].putPixelValue(x, y, (pixelLit[channelCount]==1)?255:0);
-          colocalizationCounts[colocalizationIndex]++;
+            //Pixel Analysis 
+            int pixelLit = (imageProcessor[i].getPixel(x,y) > channelThresholds[i])?1:0;
+            colocalizationIndex[i][n] = (colocalizationIndex[i][n] << 1) + pixelLit; 
+          }
         }
-        else 
-        {
-          imageProcessor[channelCount].putPixelValue(x, y, 0);
-        }
-      }   
+        colocalizationCounts[i][colocalizationIndex[i][n]]++;
       }
-      IJ.showProgress((double)n/stackSize);
-      IJ.showStatus(n+"/"+stackSize);
     }
   }
-  
-  
+
+
+
   private Boolean saveColocalizationCounts()
   {
     String toPrint = "";
-    int[] bitmasks = new int[channelCount];
+    int[] bitmasks = new int[sliceCount];
     
-    /*Create Bitmasks for each channel*/
-    for(int i=0; i<channelCount; i++)
+    /*Create Bitmasks for each slice coloc combo*/
+    for(int i=0; i<sliceCount; i++)
     {
       int shift = 1; 
       //toPrint = toPrint + channelTitles[i] + ",";
       toPrint = toPrint + Integer.toString(i) + ",";
-      bitmasks[i] = shift << (channelCount - 1 - i);
+      bitmasks[i] = shift << (sliceCount - 1 - i);
     }
     toPrint = toPrint + "pixel count\n";
     
-    for(int i=0; i < colocalizationCounts.length; i++)
+    for(int i=0; i < comboOptions; i++)
     {
-      for(int j=0; j<channelCount; j++)
+      for(int j=0; j<sliceCount; j++)
       {
         if((i & bitmasks[j]) != 0)
         {
@@ -329,7 +352,13 @@ public class Multiplex_Pixel_Colocalization_ implements PlugIn
           toPrint = toPrint + NEGATIVE + ",";
         }
       }
-      toPrint = toPrint + Integer.toString(colocalizationCounts[i]) + "\n";
+      int sumTotal = 0;
+      for (int n=0; n < stampCount; n++)
+      {
+        sumTotal += colocalizationCounts[n][i];
+        toPrint = toPrint + Integer.toString(colocalizationCounts[n][i]) + ",";
+      }
+      toPrint = toPrint + Integer.toString(sumTotal) + "\n";
     }
     IJ.showMessage(title, "Saving File");
     PrintWriter pw = null;
